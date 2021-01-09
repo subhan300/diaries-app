3175734c0062922921216e3f1f6dbab5d75538fd
Muhammad Subhan Akram <64373402+subhan300@users.noreply.github.com>
2021-01-09 23:04:48 +0500

    update it

--- src/index.tsx
+++ src/index.tsx
@@ -8,9 +8,10 @@ import { setupServer } from './services/mirage/server';
 
 import { Provider } from 'react-redux';
 import store from './store';
-if (process.env.NODE_ENV === 'development') {
-  setupServer();
-}
+// if (process.env.NODE_ENV === 'development') {
+ 
+// }
+ setupServer();
 ReactDOM.render(
   <Provider store={store}>
   <App />
